9e55babfc7d6a0a4b84029e35bef5b58e752d6f8
Viktor August Rattleff <varattleff@Viktors-MacBook-Air.local>
2024-02-12 10:26:03 +0100

    update

---
+++ person.sql
@@ -0,0 +1,4 @@
+INSERT INTO spring.person (date_of_birth, id, first_name, last_name) VALUES ('1980-07-31', 1, 'Harry', 'Potter');
+INSERT INTO spring.person (date_of_birth, id, first_name, last_name) VALUES ('1980-03-01', 2, 'Ron', 'Wealey');
+INSERT INTO spring.person (date_of_birth, id, first_name, last_name) VALUES ('1979-09-19', 3, 'Herione', 'Granger');
+INSERT INTO spring.person (date_of_birth, id, first_name, last_name) VALUES ('1980-06-05', 4, 'Draco', 'Malfoy');
---
+++ src/main/java/kea/exercise/person/controller/PersonController.java
@@ -0,0 +1,26 @@
+package kea.exercise.person.controller;
+
+import kea.exercise.person.model.Person;
+import kea.exercise.person.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PersonController {
+
+    private final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @GetMapping("/persons")
+    public List<Person> getAllPersons(){
+
+        List<Person> persons = personRepository.findAll();
+        return persons;
+    }
+}
