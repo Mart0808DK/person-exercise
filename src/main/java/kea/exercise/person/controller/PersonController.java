6c2d464856e69edf4816011fe0f7f818cddbbf49
Viktor August Rattleff <varattleff@Viktors-MacBook-Air.local>
2024-02-12 10:38:20 +0100

    part 8 complete

--- src/main/java/kea/exercise/person/controller/PersonController.java
+++ src/main/java/kea/exercise/person/controller/PersonController.java
@@ -4,9 +4,12 @@ import kea.exercise.person.model.Person;
 import kea.exercise.person.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -22,5 +25,13 @@ public class PersonController {
 
         List<Person> persons = personRepository.findAll();
         return persons;
+    }
+
+    @GetMapping("/persons/{id}")
+    public Person getPerson(@PathVariable int id){
+
+        Optional<Person> person = personRepository.findById(id);
+
+        return person.orElse(null);
     }
 }
--- src/main/java/kea/exercise/person/model/Person.java
+++ src/main/java/kea/exercise/person/model/Person.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 
